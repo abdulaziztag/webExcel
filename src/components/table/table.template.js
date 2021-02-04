c5a44ae58951cf18f83c92ab4ba97c36b17ac3a5
Abdulaziz <abdulazizmannopov07@gmail.com>
2021-02-04 21:34:02 +0500

    Finish table resize

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -2,26 +2,31 @@ const CODES = {
   A: 65,
   Z: 90
 }
-function toCell() {
+function toCell(cell, col) {
   return `
-    <div class="cell" contenteditable>
+    <div class="cell" contenteditable data-col="${col}">
       
     </div>
   `
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-    <div class="column">
+    <div class="column" data-type="resizable" data-col="${index}">
       ${col}
+      <div class="col-resize" data-resize="col"></div>
     </div>
   `
 }
 
 function createRow(index, content) {
+  const resize = index ? `<div class="row-resize" data-resize="row"></div>` : ''
   return `
-    <div class="row">
-      <div class="row-info">${index ? index : ''}</div>
+    <div class="row" data-type="resizable">
+      <div class="row-info">
+        ${index ? index : ''}
+        ${resize}
+      </div>
       <div class="row-data">${content}</div>
     </div>
   `
@@ -43,7 +48,7 @@ export function createTable(rowsCount = 15) {
 
   rows.push(createRow(null, cols))
 
-  for (let i = 0; i< rowsCount; i++) {
+  for (let i = 0; i < rowsCount; i++) {
     const cells = new Array(colsCount)
         .fill('')
         .map(toCell)
